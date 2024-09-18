de2576daf152dc3967f1fdbefa256650e36db39d
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-09-18 18:42:28 +0200

    develop branch

--- rankdom/testing/logiInTest.py
+++ rankdom/testing/logiInTest.py
@@ -6,5 +6,5 @@ class logInTest(TestCase):
     def test_creaete_user(self):
         user = LogIn(username="testuser", password="testpassword")
 
-        self.assertEqual(user.username, "tesu32sr")
+        self.assertEqual(user.username, "testuser")
         self.assertEqual(user.password, "testpassword")
