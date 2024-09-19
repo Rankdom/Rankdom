b6031098e735e541dce965a31ad38e56a5cdea90
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-09-19 14:51:15 +0200

    failed tests CI

--- rankdom/logIn.py
+++ rankdom/logIn.py
@@ -1,4 +1,4 @@
-class LogIn:
+class logIn:
     def __init__(self, username, password):
         self.username = username
         self.password = password
---
+++ rankdom/test/test_logIn.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+class logIn:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def authenticateUser(self):
+        print(f"Authenticating user with data: {self.username}")
+
+    def createUser(self):
+        return self
+
+
+class logInTest(TestCase):
+    def test_creaete_user(self):
+        user = logIn(username="testuser", password="testpassword")
+
+        self.assertEqual(user.username, "testdsfsduser")
+        self.assertEqual(user.password, "testpassword")
