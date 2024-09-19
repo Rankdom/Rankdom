0ac89ecd0e2caa4394677d55335879f83978a81d
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-09-19 12:55:25 +0200

    succes

--- rankdom/testing/logiInTest.py
+++ rankdom/testing/logiInTest.py
@@ -6,5 +6,5 @@ class logInTest(TestCase):
     def test_creaete_user(self):
         user = LogIn(username="testuser", password="testpassword")
 
-        self.assertEqual(user.username, "testus234er")
+        self.assertEqual(user.username, "testuser")
         self.assertEqual(user.password, "testpassword")
