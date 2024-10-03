b90310e91df840f46961becc178e1b6050d7fd02
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2024-10-03 17:01:06 +0200

    rankdom folder

---
+++ Backend/rankdom/serializers.py
@@ -0,0 +1,13 @@
+# import serializers from the REST framework
+from rest_framework import serializers
+
+# import the rankdom data model
+from .models import Rankdom
+
+
+# create a serializer class
+class RankdomSerializer(serializers.ModelSerializer):
+    # create a meta class
+    class Meta:
+        model = Rankdom
+        fields = ('id', 'title', 'description', 'completed')
---
+++ Backend/rankdom/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# import view sets from the REST framework
+from rest_framework import viewsets
+
+# import the TodoSerializer from the serializer file
+from .serializers import RankdomSerializer
+
+# import the Todo model from the models file
+from .models import Rankdom
+
+
+# create a class for the Todo model viewsets
+class RankdomView(viewsets.ModelViewSet):
+    # create a serializer class and
+    # assign it to the TodoSerializer class
+    serializer_class = RankdomSerializer
+
+    # define a variable and populate it 
+    # with the Todo list objects
+    queryset = Rankdom.objects.all()
