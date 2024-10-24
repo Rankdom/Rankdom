baf299bd0ebde519aaf2b438ecca1c1a865b6e54
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2024-10-24 16:11:37 +0200

    merge

---
+++ Backend/rankdom/api/Serializers.py
@@ -0,0 +1,38 @@
+from django.db import models
+from rest_framework import serializers
+
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(null=True, blank=True)
+    code=models.CharField(max_length=128, blank=True, null=False,)
+
+
+class UserRegistration(models.Model):
+    email = models.EmailField()
+    username = models.CharField(max_length=255)
+    code = models.CharField(max_length=15)
+class codeRegister(models.Model):
+    code = models.CharField(max_length=100)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username','email','code']
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {"required": False, "allow_null": True},
+            "code": {"required": False, "allow_null": True},
+            "username": {"required": False, "allow_null": True},
+
+
+        }
+
+
+class codeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = codeRegister
+        fields = ['code']
+
+
