176b3568a8aa8e8b68551d94adcbc2d539cb848e
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2024-10-31 14:57:29 +0100

    start of questions

--- Backend/rankdom/urls.py
+++ Backend/rankdom/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path,include
+from django.urls import path, include
 from django.contrib import admin
 
 from djangoProject.urls import router
 from rankdom.api.views import *
 
-
+from rankdom.api.views import Authenticate, login, home
 
 urlpatterns = [
     path("", home, name="home"),
     path("Register/", login.as_view(), name="login"),
     path("authenticator/", Authenticate.as_view(), name="authenticator"),
-    path("api/",include(router.urls),name="api"),
+    path("api/", include(router.urls), name="api"),
     path('admin/', admin.site.urls),
 
 ]
