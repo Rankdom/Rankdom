78ce606600f82063d86a68e554656367c6a9d5f2
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2024-11-07 15:16:34 +0100

    clean

--- Backend/rankdom/models.py
+++ Backend/rankdom/models.py
@@ -10,3 +10,29 @@ class Rankdom(models.Model):
     def __str__(self):
         # it will return the title
         return self.title
+
+
+
+class Profile(models.Model):
+    image = models.ImageField(upload_to='product_images/'),
+    name = models.CharField(max_length=150, blank=True, null=True),
+
+
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
+
+class CustomUser(models.Model):
+    username = models.CharField(max_length=150)
+    email = models.EmailField(null=True, blank=True)
+    code = models.CharField(max_length=128, blank=True, null=False, )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='user'
+                                , null=True, blank=True)
+
+
+
+
+class UserRegistration(models.Model):
+    email = models.EmailField()
+    username = models.CharField(max_length=255)
+    code = models.CharField(max_length=15)
--- Backend/rankdom/urls.py
+++ Backend/rankdom/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
-from rankdom.api.views import *
+from win32comext.axscript.client.framework import profile
+
+from rankdom.api.views import login,Authenticate,profile,home
 
 
 
@@ -7,5 +9,6 @@ urlpatterns = [
     path("", home, name="home"),
     path("Register/", login.as_view(), name="login"),
     path("authenticator/", Authenticate.as_view(), name="authenticator"),
+    path("profile/", profile.as_view(), name="authenticator"),
 
 ]
