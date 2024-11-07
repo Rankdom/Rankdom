15f87f5081b9bfa7ea9f3a48c37ebcae27f8b7ef
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2024-11-07 15:22:04 +0100

    added fields to questionsset

--- Backend/rankdom/models.py
+++ Backend/rankdom/models.py
@@ -1,18 +1,21 @@
+import datetime
+
 from django.db import models
 
 
 # Create your models here.
-
-
-
-
-
 class Questionsset(models.Model):
+    title = models.CharField(max_length=150)
     supercategory = models.CharField(max_length=150)
     category = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    create_date = models.DateTimeField(default=datetime.datetime.now())
     content_array = models.JSONField(max_length=400)
+
     def __str__(self):
         # it will return the title
         return self.supercategory
+
 
