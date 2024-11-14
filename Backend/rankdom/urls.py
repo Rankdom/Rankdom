1ce5346d9aac8fd9d4d89e642dc55e735a85df84
Michael Sylvest Bendtsen <54103011+Michael30502@users.noreply.github.com>
2024-11-14 14:40:48 +0100

    added image capabilities

--- Backend/rankdom/urls.py
+++ Backend/rankdom/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path("api/", include(apiRouter.urls), name="api"),
     path('admin/', admin.site.urls),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
